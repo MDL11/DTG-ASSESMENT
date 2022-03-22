95dd919a61d249dc69d9f5d3c61bca296601c2b3
MDL11 <99291190+MDL11@users.noreply.github.com>
2022-03-23 10:00:07 +1300

    added stuff

---
+++ Chat_bot.py
@@ -0,0 +1,15 @@
+# Chat bot program
+# List of random names
+# List of Pizza names
+# List of pizza prices
+# Customer details dictionary
+# Validates inputs to check if they are blank
+# Menu for pickup or delivery
+# Pick up information- name and phone number
+#Delivery information - name address and phone
+# Choose total number of pizzas - max 5
+# Pizza order- from menu - print each pizza ordered with cost
+# Print order out - including if order is deliver or pickup and names and price of each pizza - total cost including any delivery charge
+# Ability to cancel or proceed with order
+# Option for new order or to exit
+# Main function
