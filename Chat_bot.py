305cd5dd85a21b9dc3fead0c426236a2bffffc3f
MDL11 <99291190+MDL11@users.noreply.github.com>
2022-04-07 10:57:26 +1200

    Update main bot
    added ordering function into main bot

--- Chat_bot.py
+++ Chat_bot.py
@@ -12,6 +12,11 @@ shake_names = ['Apple','Chocolate','Watermelon','Strawberry','Pear','Caramel','O
 # List of Boba prices
 shake_prices = [8.50, 8.50, 8.50, 8.50, 8.50, 8.50, 8.50, 13.50, 13.50, 13.50, 13.50, 13.50]
 
+# List to store ordered shakes
+order_list =[]
+# List to store shake prices
+order_cost = []
+
 # Customer details dictionary
 customer_details = {}
 
@@ -112,32 +117,47 @@ def delivery_info():
     print (customer_details['suburb']) 
 
 
-
-
-
-
-
 # shake menu
 def menu():
     number_shake = 12
-    for count in range (number_shake):
+    for count in range (number_shake) :
          print("{} {} ${:.2f}" .format(count+1, shake_names[count],shake_prices[count]))
 
+# Pizza order- from menu - print each pizza ordered with cost 
+def order_shake():
+ # ask for total number of shakes for order
+ num_shakes = 0
+
+ while True:
+    try:
+        num_shakes = int(input("How many shakes do you want to order "))
+        if num_shakes >= 1 and num_shakes <= 12:
+            break
+        else:
+            print("Your order must be between 1 and 12")
+    except ValueError:
+            print ("That is not a valid number")
+            print("please enter 1 or 12)")
 
 
-
-
-
-
-
-# Pizza order- from menu - print each pizza ordered with cost
-
-
-
-
-
-# Print order out - including if order is deliver or pickup and names and price of each pizza - total cost including any delivery charge
-
+#choose shake from menu
+ for item in range(num_shakes):
+    while num_shakes > 0:
+        while True:
+                    try:
+                         shakes_ordered = int(input("Please choose your shake by selecting a number from the menu "))
+                         if shakes_ordered >= 1 and shakes_ordered <= 12:
+                             break
+                         else:
+                          print("Your order must be between 1 and 12")
+                    except ValueError:
+                        print ("That is not a valid number")
+                        print("please enter number between 1-12) ")
+        shakes_ordered = shakes_ordered -1
+        order_list.append(shake_names[shakes_ordered])
+        order_cost.append(shake_prices[shakes_ordered])
+        print("{} ${:.2f}" .format(shake_names[shakes_ordered],shake_prices[shakes_ordered]))
+        num_shakes =  num_shakes-1
 
 
 
@@ -166,6 +186,7 @@ def main():
     welcome()
     order_type()
     menu()
+    order_shake()
     
 
 
