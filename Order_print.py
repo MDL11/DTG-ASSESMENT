04b02ea38857a5670b43ffec0cb2114bd50b552a
MDL11 <99291190+MDL11@users.noreply.github.com>
2022-04-12 11:59:12 +1200

    Update Order_print.py
    turn to function

--- Order_print.py
+++ Order_print.py
@@ -1,17 +1,22 @@
 #list to store ordered shakes
-order_list = ['Apple','Chocolate','Watermelon','Strawberry','Pear','Caramel','Orange','Mango', 'Rockmelon','Banana','Apricot','pineaple']
+order_list = ['Apple','Watermelon','Orange','pineaple']
 #list to store pizza prices
-order_cost = [8.50, 8.50, 8.50, 8.50, 8.50, 8.50, 8.50, 13.50, 13.50, 13.50, 13.50, 13.50]
+order_cost = [8.50, 8.50, 8.5, 13.50]
 
 cust_details = {'name':'Jeff','phone':'0213345656', 'house':'5343', 'street':'Harry', 'suburb':'Howick'}
 
+def print_order():
+    total_cost = sum(order_cost)
+    print ("Customer Details")
+    print(f"Customer Name: {cust_details['name']} /nCustomer Phone: {cust_details['phone']} /nCustomer Address: {cust_details['house']} {cust_details['street']} {cust_details['suburb']}")
+    print()
+    print("Order Details")
+    count = 0
+    for item in order_list:
+        print("Ordered: {} Cost ${:.2f}".format(item, order_cost[count]))
+        count = count+1
+    print()
+    print("total Order Cost")
+    print(f"${total_cost:.2f}")
 
-
-#print("\n",cust_details['name],"\n",cust_details['phone], "\n,cust_details['house'], "\n,cust_details['street], "\n",cust_details['suburb])
-
-print("\n Customer Name: {} Customer Phone:\n{} Customer House Number:\n{} Customer Street Name:\n{} Customer Suburb:\n{}".format( cust_details['name'],cust_details['phone'],cust_details['house'],cust_details['street'],cust_details['suburb']))
-
-count = 0
-for item in order_list:
-    print("Ordered: {} Cost ${:.2f}".format(item, order_cost[count]))
-    count = count+1
+print_order()
